604ba8515c03b5331ab5ed9a171f1e338e16be7f
pavben <github-public@pavelbennett.com>
2013-08-19 12:43:54 -0700

    removed fs-extra due to a double-callback scenario in readJson

--- config.js
+++ config.js
@@ -1,19 +1,27 @@
 require('./utils.js').installGlobals();
 
-var fs = require('fs-extra');
+var fs = require('fs');
 
 exports.data = {};
 
 function load(configFilePath, cb) {
-	fs.readJson(configFilePath, check(
+	fs.readFile(configFilePath, check(
 		function(err) {
+			console.log('readFile returned failure!', err);
 			if (err.code === 'ENOENT') {
 				cb();
 			} else {
 				cb(err);
 			}
 		},
-		function(configObj) {
+		function(data) {
+			var configObj = null;
+			try {
+				configObj = JSON.parse(data);
+			} catch(err) {
+				cb(err);
+			}
+
 			console.log('Read config', configObj);
 
 			exports.data = configObj;
