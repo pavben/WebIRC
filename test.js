a51f8e7a0add4421833d1d4bcc5e44593ca8ff85
pavben <github-public@pavelbennett.com>
2013-09-20 13:47:07 -0700

    added a test to join 1000 clients to a channel

--- test.js
+++ test.js
@@ -1,13 +1,23 @@
 function runTest(master, testId) {
 	if (testId === '1') {
 		if (master.activeWindow.type === 'channel') {
-			var channelOrQuery = master.activeWindow.object;
-
 			master.user.applyStateChange('Join', master.activeWindow.windowPath, {
 				nick: 'wezirc',
 				user: 'user',
 				host: 'host'
 			});
+		} else {
+			master.showError('Must be used in a channel');
+		}
+	} else if (testId === '2') {
+		if (master.activeWindow.type === 'channel') {
+			for (var i = 0; i < 1000; i++) {
+				master.user.applyStateChange('Join', master.activeWindow.windowPath, {
+					nick: 'u' + i,
+					user: 'user',
+					host: 'host'
+				});
+			}
 		} else {
 			master.showError('Must be used in a channel');
 		}
